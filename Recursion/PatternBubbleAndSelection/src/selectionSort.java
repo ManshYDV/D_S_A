933222ec317b3b6f21217a62859f0cb896e65e83
Mansh <manishyadav7801850@gmail.com>
2023-11-23 15:52:36 +0530

    Today I learned recursion pattern questions and sorting in recursion.

--- Recursion/PatternBubbleAndSelection/src/selectionSort.java
+++ Recursion/PatternBubbleAndSelection/src/selectionSort.java
@@ -1,29 +1,27 @@
 import java.util.Arrays;
 
 public class selectionSort {
-    private static void sol(int []arr, int i, int j, int max){
-        if(i==0){
+    private static void sol(int[] arr, int i, int j, int max) {
+        if (i == 0) {
             return;
         }
-        if(j<i){
-            if(arr[j] > arr[max]){
-                sol(arr, i,j-1, j);
+        if (j < i) {
+            if (arr[j] > arr[max]) {
+                sol(arr, i, j - 1, j);
+            } else {
+                sol(arr, i, j + 1, max);
             }
-            else{
-                sol(arr, i, j+1, max);
-            }
-        }
-        else{
-            int temp=arr[max];
-            arr[max]=arr[i-1];
-            arr[i-1]=temp;
-            sol(arr, i-1,0,0);
+        } else {
+            int temp = arr[max];
+            arr[max] = arr[i - 1];
+            arr[i - 1] = temp;
+            sol(arr, i - 1, 0, 0);
         }
     }
 
     public static void main(String[] args) {
-        int []arr={5,4,3,2,1};
-    sol(arr,arr.length,0,0);
+        int[] arr = {5, 4, 3, 2, 1};
+        sol(arr, arr.length, 0, 0);
         System.out.println(Arrays.toString(arr));
     }
 }
