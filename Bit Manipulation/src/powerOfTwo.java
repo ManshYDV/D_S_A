cbc66c208a1482dfe10c83d1225be9dcb4ea8328
Mansh <manishyadav7801850@gmail.com>
2024-01-08 07:02:58 +0530

    7/11/24 Recursion basics

--- Bit Manipulation/src/powerOfTwo.java
+++ Bit Manipulation/src/powerOfTwo.java
@@ -1,11 +1,12 @@
 public class powerOfTwo {
     public static void main(String[] args) {
-        int n=64;
+        int n=16;
         int count=0;
         while(n>0){
             n=n>>1;
             count++;
         }
+        Math.floor(4);
         System.out.println("its a power of 2^"+count);
         if((n&(n-1))==0){
             System.out.println("Its a power of two");
